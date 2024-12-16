690e7f6f4ff408c6ff51f80e67d092418a646185
wayne0299 <wayne02990@gmail.com>
2024-12-16 20:58:06 +0900

    2차 프로젝트 회원가입 및 로그인 구현.
    Swagger API
    
    item,cart test 는 최종에 가능할듯.
    
    판매자, my 작업 필요

---
+++ src/main/java/com/example/coupang/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.coupang.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // application.yml에서 CORS 허용 URL을 가져옵니다.
+    @Value("${app.cors.allowed-origin}")  // CORS 설정 값 가져오기
+    private String allowedOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                .allowedOrigins(allowedOrigin) // 설정된 URL만 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 인증 정보 허용
+    }
+}
