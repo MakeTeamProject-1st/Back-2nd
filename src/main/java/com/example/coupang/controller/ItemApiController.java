95a466538de4d6c351c6d749056d00978059be18
wayne0299 <wayne02990@gmail.com>
2024-12-16 20:51:51 +0900

    2차 프로젝트 회원가입 및 로그인 구현.
    Swagger API
    
    item,cart test 는 최종에 가능할듯.
    
    판매자, my 작업 필요

--- src/main/java/com/example/coupang/controller/ItemApiController.java
+++ src/main/java/com/example/coupang/controller/ItemApiController.java
@@ -5,10 +5,7 @@ import com.example.coupang.service.ItemService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
 
 @Tag(name = "Item API", description = "상품관련 API")
